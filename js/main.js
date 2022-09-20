b9f7cc14632372f3ebf667faa0ac4eb3c108ed31
Saskia Peach <saskiapeach@gmail.com>
2022-09-20 22:04:04 +0100

    added 2 rows of bricks, implement collision detection

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const canvas = document.getElementById('myCanvas');
 const paint = canvas.getContext('2d');
 
+const scoreChart = document.getElementById('score-chart')
+
 //define a location for the ball
 let ballPositionX = canvas.width / 2;
-let ballPositionY = canvas.height-40;
+let ballPositionY = canvas.height-30;
 
 //define the moving figures for my ball
 let directionX = 2;
@@ -24,10 +26,22 @@ let leftPressed = false;
 
 let brickHeight = 15
 let brickWidth = 80
+let brickPositionX = 10
 let brickPositionY = 10
 let brickPadding = 12
 let brickColumns = 7
+let brickRows = 2
 let brickLeftMargin = 20
+
+let bricks = [];
+
+for (let i = 0; i < brickColumns; i++){
+    bricks[i] = [];
+    for(j = 0; j<brickRows; j++){
+        bricks[i][j] = {x:0, y:0, status:1};
+    }
+}
+console.log(bricks)
 
 
 document.addEventListener("keydown", keyDownHandler);
@@ -57,7 +71,21 @@ function keyDownHandler(event) {
     }
   }
 
-  
+  function detectCollision(){
+    for(let i = 0; i<brickColumns; i++){
+        for(let j = 0; j<brickRows; j++){
+            let singleBrick = bricks[i][j];
+            if(singleBrick.status === 1){
+            if(ballPositionX  > singleBrick.x && ballPositionX  < singleBrick.x + brickWidth && ballPositionY > singleBrick.y && ballPositionY < singleBrick.y + brickHeight){
+                directionY = -directionY
+            singleBrick.status = 0;}
+            }
+        }
+    
+    }
+     
+}
+
 function drawBall() {
     paint.beginPath();
     paint.arc(ballPositionX, ballPositionY, ballRadius, 0, Math.PI * 2);
@@ -69,15 +97,24 @@ function drawBall() {
   
   function drawBricks() {
     for(let i=0; i<brickColumns; i++){
-        let brickPositionX = (i*(brickWidth+brickPadding)) + brickLeftMargin
-        paint.beginPath();
-        paint.rect(brickPositionX, brickPositionY, brickWidth, brickHeight);
-        paint.fillStyle = "#000000";
-        paint.fill();
-        paint.closePath();
-        brickPositionX += brickPadding+brickWidth
+        for(let j=0; j<brickRows; j++){
+            if(bricks[i][j].status ===1){
+                let brickX = (i * (brickWidth + brickPadding)) + brickLeftMargin
+                let brickY = (j * (brickHeight + brickPadding));
+                bricks[i][j].x = brickX;
+                bricks[i][j].y = brickY;
+                paint.beginPath();
+                paint.rect(brickX, brickY, brickWidth, brickHeight);
+                paint.fillStyle = "#000000";
+                paint.fill();
+             paint.closePath();
+            }
+        }
+
     }
   }
+  
+  
 
   
 
@@ -88,6 +125,7 @@ function drawBall() {
     drawBall();
     drawPaddle();
     drawBricks();
+    detectCollision();
 
 
     if(rightPressed === true) {
@@ -118,7 +156,7 @@ function drawBall() {
             directionY = -directionY;
         } else {
         console.log('game is over');
-        clearInterval(interval); 
+        location.href = "gameover.html"; 
          }
       }
     
