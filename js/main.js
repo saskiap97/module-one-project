bfa6e5648452955d168b51bfabf65116a89cbece
Saskia Peach <saskiapeach@gmail.com>
2022-09-20 12:02:52 +0100

    cleared code, starting on bricks

--- js/main.js
+++ js/main.js
@@ -1,90 +1,122 @@
 const canvas = document.getElementById('myCanvas');
 const paint = canvas.getContext('2d');
 
-let x = canvas.width / 2;
-let y = canvas.height - 30;
-let dx = 2;
-let dy = -2;
+//define a location for the ball
+let ballPositionX = canvas.width / 2;
+let ballPositionY = canvas.height-40;
 
-const ballRadius = 10;
+//define the moving figures for my ball
+let directionX = 2;
+let directionY = -2;
 
-const paddleHeight = 10;
+//define size of ball and size of paddle
+const ballRadius = 8;
+const paddleHeight = 8;
 const paddleWidth = 75;
-let paddleX = (canvas.width - paddleWidth) / 2
+
+//define starting paddle position
+let paddlePosition = (canvas.width - paddleWidth) / 2
 
 let rightPressed = false;
 let leftPressed = false;
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
+//define bricks characters
 
-function keyDownHandler(e) {
-    if (e.key === "Right" || e.key === "ArrowRight") {
+const brickHeight = 15
+const brickWidth = 60
+const brickPositionX = 10
+const brickPositionY = 10
+
+document.addEventListener("keydown", keyDownHandler);
+document.addEventListener("keyup", keyUpHandler);
+
+function drawPaddle() {
+    paint.beginPath();
+    paint.rect(paddlePosition, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    paint.fillStyle = "#FF9C33";
+    paint.fill();
+    paint.closePath();
+  }
+
+function keyDownHandler(event) {
+    if (event.key === "ArrowRight") {
       rightPressed = true;
-    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    } else if (event.key === "ArrowLeft") {
       leftPressed = true;
     }
   }
-  
-  function keyUpHandler(e) {
-    if (e.key === "Right" || e.key === "ArrowRight") {
+
+  function keyUpHandler(event) {
+    if (event.key === "ArrowRight") {
       rightPressed = false;
-    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    } else if (event.key === "ArrowLeft") {
       leftPressed = false;
     }
   }
 
-  function drawBall() {
+  
+function drawBall() {
     paint.beginPath();
-    paint.arc(x, y, ballRadius, 0, Math.PI * 2);
-    paint.fillStyle = "#0095DD";
+    paint.arc(ballPositionX, ballPositionY, ballRadius, 0, Math.PI * 2);
+    paint.fillStyle = "#4B2804";
     paint.fill();
     paint.closePath();
   }
 
-  function drawPaddle() {
+  function drawBricks() {
     paint.beginPath();
-    paint.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-    paint.fillStyle = "#0095DD";
+    paint.rect(brickPositionX, brickPositionY, brickWidth, brickHeight);
+    paint.fillStyle = "#000000";
     paint.fill();
     paint.closePath();
   }
 
-  function draw() {
+ 
+
+
+  function gamePlay() {
     paint.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     drawPaddle();
-    
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-        dx = -dx;
+    drawBricks();
+
+    if(rightPressed === true) {
+        paddlePosition += 7;
+        if(paddlePosition > canvas.width-paddleWidth){
+    paddlePosition = (canvas.width-paddleWidth)}
     }
-    if (y + dy < ballRadius) {
-        dy = -dy;
-      } else if (y + dy > canvas.height - ballRadius) {
-        if(x > paddleX && x < paddleX + paddleWidth){
-            dy = -dy;
+    else if(leftPressed === true) {
+        paddlePosition -= 7;
+        if (paddlePosition < 0){
+            paddlePosition = 0;
+        }
+    }
+    
+    // implementing bouncing off the walls 
+    
+    if(ballPositionX + directionX > canvas.width-ballRadius || ballPositionX + directionX < ballRadius) {
+        directionX = -directionX;
+    }
+    if (ballPositionY + directionY < ballRadius) {
+        directionY = -directionY;
+      } 
+      
+    // implement bouncing off the paddle and setting end game
+
+      else if (ballPositionY + directionY > canvas.height - ballRadius) {
+        if(ballPositionX > paddlePosition && ballPositionX < paddlePosition + paddleWidth){
+            directionY = -directionY;
         } else {
         console.log('game is over');
-        clearInterval(interval); // Needed for Chrome to end game
+        clearInterval(interval); 
          }
       }
     
-    if(rightPressed) {
-        paddleX += 7;
-        if (paddleX + paddleWidth > canvas.width){
-            paddleX = canvas.width - paddleWidth;
-        }
-    }
-    else if(leftPressed) {
-        paddleX -= 7;
-        if (paddleX < 0){
-            paddleX = 0;
-        }
-    }
-    
-    x += dx;
-    y += dy;
+   
+    //implementing moving of ball
+    ballPositionX += directionX;
+    ballPositionY += directionY;
 }
 
-const interval = setInterval(draw, 10);
+const interval = setInterval(gamePlay, 10);
 
