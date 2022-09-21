c07ed2a16172f63db4c18cc67e05e14e04716178
Saskia Peach <saskiapeach@gmail.com>
2022-09-21 15:50:09 +0100

    added scoring and some styling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 const canvas = document.getElementById('myCanvas');
 const paint = canvas.getContext('2d');
 
+let background = new Image();
+background.src = "../images/paper-background.jpeg";
+paint.drawImage(background,0,0)
+
 const scoreChart = document.getElementById('score-chart')
+
+var randomColor = Math.floor(Math.random()*16777215).toString(16)
 
 //define a location for the ball
 let ballPositionX = canvas.width / 2;
@@ -24,14 +30,19 @@ let leftPressed = false;
 
 //define bricks characters
 
-let brickHeight = 15
-let brickWidth = 80
+let brickHeight = 20
+let brickWidth = canvas.width/8
 let brickPositionX = 10
 let brickPositionY = 10
 let brickPadding = 12
 let brickColumns = 7
-let brickRows = 2
-let brickLeftMargin = 20
+let brickRows = 6
+let brickLeftMargin = 10
+let brickTopMargin = 15
+
+let myRed = Math.floor(Math.random() * 256);
+let myGreen = Math.floor(Math.random() * 256);
+let myBlue = Math.floor(Math.random() * 256);
 
 let bricks = [];
 
@@ -47,7 +58,11 @@ for (let i = 0; i < brickColumns; i++){
 
 let counter = 0
 
-console.log(bricks)
+let images = {
+    background: new Image()
+}
+
+images.background.src = '../images/paper-background.jpeg'
 
 
 document.addEventListener("keydown", keyDownHandler);
@@ -55,8 +70,8 @@ document.addEventListener("keyup", keyUpHandler);
 
 function drawPaddle() {
     paint.beginPath();
-    paint.rect(paddlePosition, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-    paint.fillStyle = "#FF9C33";
+    paint.rect(paddlePosition, canvas.height - paddleHeight - 10, paddleWidth, paddleHeight);
+    paint.fillStyle = "#C0C0C0";
     paint.fill();
     paint.closePath();
   }
@@ -77,6 +92,7 @@ function keyDownHandler(event) {
     }
   }
 
+
   function detectCollision(){
     
     for(let i = 0; i<brickColumns; i++){
@@ -86,10 +102,15 @@ function keyDownHandler(event) {
             if(ballPositionX  > singleBrick.x && ballPositionX  < singleBrick.x + brickWidth && ballPositionY > singleBrick.y && ballPositionY < singleBrick.y + brickHeight){
                 directionY = -directionY
             singleBrick.present = 'no';
-            counter++;
+            counter+=10;
+            if(counter > (brickRows * brickColumns)*10){
+
+                location.href = "youwin.html"
+            }
+            
                 }
                 
-            scoreChart.innerText =`your score is ${counter}`
+            scoreChart.innerText =`Your score is ${counter} `
             }
             
         }
@@ -101,7 +122,7 @@ function keyDownHandler(event) {
 function drawBall() {
     paint.beginPath();
     paint.arc(ballPositionX, ballPositionY, ballRadius, 0, Math.PI * 2);
-    paint.fillStyle = "#4B2804";
+    paint.fillStyle = "#d01c1f";
     paint.fill();
     paint.closePath();
   }
@@ -111,13 +132,13 @@ function drawBall() {
     for(let i=0; i<brickColumns; i++){
         for(let j=0; j<brickRows; j++){
             if(bricks[i][j].present ==='yes'){
-                let brickX = (i * (brickWidth + brickPadding)) + brickLeftMargin
-                let brickY = (j * (brickHeight + brickPadding));
+                let brickX = (i * (brickWidth + brickPadding)) + brickLeftMargin;
+                let brickY = (j * (brickHeight + brickPadding)) + brickTopMargin;
                 bricks[i][j].x = brickX;
                 bricks[i][j].y = brickY;
                 paint.beginPath();
-                paint.rect(brickX, brickY, brickWidth, brickHeight);
-                paint.fillStyle = "#000000";
+                paint.roundRect(brickX, brickY, brickWidth, brickHeight, [10]);
+                paint.fillStyle = 'rgb('+ myRed + ',' + myGreen + ',' + myBlue + ')';
                 paint.fill();
              paint.closePath();
             }
@@ -133,6 +154,7 @@ function drawBall() {
  
 
   function gamePlay() {
+    paint.drawImage(images.background,0,0,canvas.width, canvas.height)
     paint.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     drawPaddle();
@@ -163,7 +185,7 @@ function drawBall() {
       
     // implement bouncing off the paddle and setting end game
 
-      else if (ballPositionY + directionY > canvas.height - ballRadius) {
+      else if (ballPositionY + directionY +15> canvas.height - ballRadius) {
         if(ballPositionX > paddlePosition && ballPositionX < paddlePosition + paddleWidth){
             directionY = -directionY;
         } else {
